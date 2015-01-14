c70eaa1a511f0448d0fecdd88453b54bf8cd8504
tzookb <tzookb@gmail.com>
2015-01-14 18:44:03 +0200

    changed the nginx conf

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -17,7 +17,7 @@ block="server {
     location = /robots.txt  { access_log off; log_not_found off; }
 
     access_log off;
-    error_log  /var/log/nginx/$1-error.log error;
+    error_log  /var/log/nginx/$1/error.log error;
 
     error_page 404 /index.php;
 
@@ -37,6 +37,7 @@ block="server {
 "
 
 echo "$block" > "/etc/nginx/sites-available/$1"
+sudo mkdir /var/log/nginx/$1
 ln -fs "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
 service nginx restart
 service php5-fpm restart
