2423fdc54c71f8ff69051c239942dd8e7e156353
tzookb <tzookb@initialrewards.com>
2015-01-20 16:51:24 +0200

    added phantomjs

--- bash/phantomjs.sh
+++ bash/phantomjs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo " -------------------------  phantomjs  ------------------------- "
-#wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-#tar jxf phantomjs-1.9.7-linux-x86_64.tar.bz2
-#mv phantomjs-1.9.7-linux-x86_64 phantomjs
-#sudo mv phantomjs/bin/phantomjs /usr/local/bin/phantomjs
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+tar jxf phantomjs-1.9.8-linux-x86_64.tar.bz2
+mv phantomjs-1.9.7-linux-x86_64 phantomjs
+sudo mv phantomjs/bin/phantomjs /usr/local/bin/phantomjs
+rm -rf phantomjs
