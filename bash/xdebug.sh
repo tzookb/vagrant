c72c0b27f12a92d80701b0c7a2563fc9b4a148e2
tzookb <tzookb@initialrewards.com>
2015-01-20 09:59:07 +0200

    added fix to xdebug

--- bash/xdebug.sh
+++ bash/xdebug.sh
@@ -5,9 +5,19 @@ echo " -------------------------  Installing and configuring Xdebug  -----------
 sudo apt-get install -y php5-xdebug
 
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+zend_extension=xdebug.so
+xdebug.remote_enable=1
+xdebug.remote_host=localhost
+xdebug.remote_port=9000
+xdebug.remote_handler=dbgp
 xdebug.scream=1
 xdebug.cli_color=1
 xdebug.show_local_vars=1
+xdebug.default_enable=1
+xdebug.idekey="vagrant"
+xdebug.profiler_enable=1
+xdebug.remote_autostart=1
+xdebug.remote_connect_back=1
 EOF
 
 echo " -------------------------  YOU NEED TO RESTART SERVER  ------------------------- "
